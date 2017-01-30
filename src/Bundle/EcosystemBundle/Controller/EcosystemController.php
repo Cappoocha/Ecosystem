3d5aed3cfa4585f1e68a5226c0603f687f2e3a8f
cappuccino <katerinacappuccino@gmail.com>
2017-01-30 22:03:03 +0400

    Новая структура

---
+++ src/Bundle/EcosystemBundle/Controller/EcosystemController.php
@@ -0,0 +1,12 @@
+<?php
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @author Катерина
+ * Date: 30.01.2017
+ * Time: 22:00
+ */
+class EcosystemController extends Controller
+{
+
+}
